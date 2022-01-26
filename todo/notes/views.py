0d5fbcae8ed888f83589325f36e82b13e86b9fd5
m0shanya <funnytimka@gmail.com>
2022-01-26 21:11:04 +0300

    add todo classwork

---
+++ todo/notes/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from notes.forms import AddNoteForm
+from notes.models import Note
+
+
+def notes_index(request):
+    notes = []
+    if request.method == "POST":
+        form = AddNoteForm(request.POST)
+        if form.is_valid():
+            Note.objects.create(
+                title=form.cleaned_data["title"], text=form.cleaned_data["text"]
+            )
+    else:
+        form = AddNoteForm()
+    notes = Note.objects.all()
+    return render(request, "note_list.html", {"notes": notes, "form": form})
